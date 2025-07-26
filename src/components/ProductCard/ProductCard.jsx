098fa0def298a50bf199242ad08373ee3712b6b9
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-07-26 20:27:23 +0530

    Update ProductCard.jsx

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -8,35 +8,36 @@ import { addToCart } from "../../app/features/cart/cartSlice";
 const ProductCard = ({ title, productItem }) => {
   const dispatch = useDispatch();
   const router = useNavigate();
-  const handelClick = () => {
+
+  const handleClick = () => {
     router(`/shop/${productItem.id}`);
   };
-  const handelAdd = (productItem) => {
+
+  const handleAdd = () => {
     dispatch(addToCart({ product: productItem, num: 1 }));
     toast.success("Product has been added to cart!");
   };
+
   return (
     <Col md={3} sm={5} xs={10} className="product mtop">
-      {title === "Big Discount" ? (
+      {title === "Big Discount" && (
         <span className="discount">{productItem.discount}% Off</span>
-      ) : null}
+      )}
       <img
         loading="lazy"
-        onClick={() => handelClick()}
+        onClick={handleClick}
         src={productItem.imgUrl}
-        alt=""
+        alt={productItem.productName}
       />
       <div className="product-like">
         <ion-icon name="heart-outline"></ion-icon>
       </div>
       <div className="product-details">
-        <h3 onClick={() => handelClick()}>{productItem.productName}</h3>
+        <h3 onClick={handleClick}>{productItem.productName}</h3>
         <div className="rate">
-          <i className="fa fa-star"></i>
-          <i className="fa fa-star"></i>
-          <i className="fa fa-star"></i>
-          <i className="fa fa-star"></i>
-          <i className="fa fa-star"></i>
+          {[...Array(5)].map((_, i) => (
+            <i key={i} className="fa fa-star"></i>
+          ))}
         </div>
         <div className="price">
           <h4>₹{productItem.price}</h4>
@@ -44,7 +45,7 @@ const ProductCard = ({ title, productItem }) => {
             aria-label="Add"
             type="submit"
             className="add"
-            onClick={() => handelAdd(productItem)}
+            onClick={handleAdd}
           >
             <ion-icon name="add"></ion-icon>
           </button>
